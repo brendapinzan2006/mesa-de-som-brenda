03e64327fc50b34c9f0bfbc277934e62d5c630ff
brendapinzan2006 <129403328+brendapinzan2006@users.noreply.github.com>
2023-10-04 08:45:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert('Olá mundo');
+function tocaSomPom () {
+    document.querySelector('.tecla_pom');
+}
+
+document.querySelector('#som_tecla_pom').onclick = tocaSomPom
+
+function tocaSomClap () {
+    document.querySelector('.tecla_clap');
+}
+
+document.querySelector('#som_tecla_clap').onclick = tocaSomClap
